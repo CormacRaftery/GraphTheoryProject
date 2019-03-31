759203fbfb20f1631d1470bc89ab90468691ec5c
CormacRaftery <cormacinho@hotmail.com>
2019-03-31 11:11:59 +0100

    Create Shunt.py

---
+++ Shunt.py
@@ -0,0 +1,30 @@
+# Cormac Raftery
+#Shunting Yard Algorithm
+
+def shunt(infix):
+
+    specials = {'*': 50, '.': 40, '|': 30}
+
+    pofix = ""
+    stack = ""
+
+    for c in infix:
+        if c == '(':
+            stack = stack + c
+        elif c ==')':
+            while stack[-1] != '(':
+                pofix, stack = pofix + stack[-1],stack[:-1]
+            stack = stack[:-1]
+        elif c in specials:
+            while stack and specials.get(c,0) <= specials.get(stack[-1],0):
+                pofix, stack = pofix + stack[-1],stack[:-1]
+            stack = stack + c
+        else:
+            pofix=pofix+c
+
+    while stack:
+        pofix,stack = pofix + stack[-1],stack[:-1]
+
+    return pofix
+
+print(shunt("(a.b)|(c*.d)"))
